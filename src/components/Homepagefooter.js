f2445dc7416533e20df8447d8d3585df85f0e141
Abhijit808 <abhijitrayarao@gmail.com>
2022-06-19 19:22:27 +0530

    commit1

--- src/components/Homepagefooter.js
+++ src/components/Homepagefooter.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import footerimg from '../images/Untitled.svg'
+// import footerimg from '../images/Untitled.svg'
 const Homepagefooter = () => {
   return (
-    <div className='footers'>
-      <img src={footerimg} alt="" />
+    <div className='footer'>
     </div>
   )
 }
