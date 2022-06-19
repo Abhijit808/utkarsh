55d63c47e59b10c0175b7c5da17d32eee86bb15d
Abhijit808 <abhijitrayarao@gmail.com>
2022-06-19 19:27:31 +0530

    commit2

--- src/components/Aboutcard.js
+++ src/components/Aboutcard.js
@@ -10,7 +10,7 @@ const Aboutcard = () => {
           <img src={cardimg_1} alt="" />
         </div>
         <div className="information center">
-          <p className="mainpara w_color ">
+          <p className="mainpara1 w_color ">
             Pendekanti Institute of Management (PIM) is established in 1991 and is the youngest among the academic institutions started by Vasavi Academy of Education (VAE).
             Pendekanti institute of management  is one of the top colleges for MBA in Hyderabad
             In PIM we have  great teaching  faculty and a very good and favourable enviornmet for students to grow and  be ready for the corporate competitive world.
@@ -22,7 +22,7 @@ const Aboutcard = () => {
           <img src={cardimg_2} alt="" />
         </div>
         <div className="information center">
-          <p className="mainpara w_color ">
+          <p className="mainpara1 w_color ">
             PIM times is a student oriented magazine which represents the work of students of this educational institution  and general information which is helpful for our career.
             We also have our faculty member's  enlighting us with some wonderful articles every month
             Not only does our pim time has education related stuff but also some really good entertainment column which can be enjoyed by everyone.
